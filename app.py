d9a93c0ce01bb23e2374db4698de325aad922ad1
andrewmsweeney <138390700+andrewmsweeney@users.noreply.github.com>
2023-07-02 18:46:49 +0000

    Added html/css

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # flask is a module (always lowercase)
 # Flask is a class inside the module
-from flask import Flask
+from flask import Flask, render_template
 # app is an object of the class Flask (creates a flask application and assigning it to app)
 app = Flask(__name__)
 
 # register a route to application (/ is homepage)
 @app.route("/")
-def hello_wold():
-  return "Hello, Andrew!"
+def hello_world():
+  return render_template('home.html')
 
 # we've defined a function that returns hello world, but by adding the decorator (@) when the url / is accessed it
 # shows the return from the function
